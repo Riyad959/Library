06d83d1f6ba8c62cb84ce45e02d8c616d7c9a6a6
riyad959 <riyadul15-4828@diu.edu.bd>
2025-07-21 20:28:47 +0600

    create home and student user

---
+++ project-folder/user/contactus.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>Library Management System</title>
+	<link rel="icon" type="image/png" href="dist/img/mm.png">
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="dist/css/fontawesome-all.min.css">
+	<link rel="stylesheet" href="dist/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="dist/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="dist/css/animate.css">
+	<link rel="stylesheet" href="dist/css/main.css">
+	<link href="https://fonts.googleapis.com/css?family=Exo+2:400,500,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+</head>
+
+<body>
+	<div class="header">
+		<div class="container">
+			<div class="row">
+				<div class="col-3">
+					<div class="logo">
+						<img src="dist/img/1.png" alt="logo">
+					</div>
+				</div>
+				<div class="col-9">
+					<div class="header-right">
+						<ul>
+							<li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							<li><a href=""><i class="fab fa-linkedin"></i></a></li>
+							<li><a href="../index.php">Home</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contactinfo">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<div class="left">
+						<h3>Send Us a Message</h3>
+						<form action="">
+							<input type="text" placeholder="Your Name">
+							<input type="email" placeholder="Your E-mail">
+							<input type="digit" placeholder="Phone Number">
+							<textarea placeholder="Your Message" id="" cols="60" rows="5"></textarea>
+							<input type="submit" value="SEND A MESSAGE">
+						</form>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="right">
+						<h3>Contact Info</h3>
+						<p>This Library Management System is designed to manage and organize library resources
+							efficiently. Also it provides a user-friendly interface for both librarians and users. All
+							the users can access the library book, search for books, and manage their accounts easily.
+						</p>
+
+						<div class="box">
+							<p><i class="fas fa-home"></i><span>H #123 Uttara, Dhaka</span></p>
+							<p><i class="far fa-envelope"></i><span>riyadulislam959@gmail.com</span></p>
+							<p><i class="fas fa-phone"></i><span>(+880) 123 456 7890</span></p>
+							<p><i class="fas fa-user"></i><span>Developer:</span></p>
+							<div class="container mt-5">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="card">
+											<img src="./dist/img/ME.png" alt="Developer 1"
+												style="width:120px; height:120px; object-fit:cover; margin-right:5px;">
+											<div>
+												<h4>Riyadul Islam</h4>
+												<p>Front-End Developer & Back-End Developer</p>
+												<a href="#"><i class="fab fa-facebook-f"></i></a>
+												<a href="#"><i class="fab fa-twitter"></i></a>
+												<a href="#"><i class="fab fa-linkedin"></i></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<br><br>
+							<p><i class="fas fa-user"></i><span>Developer:</span></p>
+							<div class="container mt-5">
+								<div class="row">
+									<div class="col-md-6">
+										<div class="card">
+											<img src="./dist/img/moin.jpeg" alt="Developer 2"
+												class="img-fluid rounded-circle"
+												style="width:120px; height:120px; object-fit:cover; margin-right:5px;">
+											<div>
+												<h4>Moin Hasan</h4>
+												<p>Front-End Developer & Back-End Developer</p>
+												<a href="#"><i class="fab fa-facebook-f"></i></a>
+												<a href="#"><i class="fab fa-twitter"></i></a>
+												<a href="#"><i class="fab fa-linkedin"></i></a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+
+
+			</div>
+		</div>
+	</div>
+	<br>
+	<div class="footer text-center">
+		<p style="margin: 20px; font-weight: bold;"><span>&copy; All rights reserved to Riyad959</span></p>
+	</div>
+
+	<script src="dist/js/jquery-2.2.4.min.js"></script>
+	<script src="dist/js/bootstrap.min.js"></script>
+	<script src="dist/js/owl.carousel.min.js"></script>
+	<script src="dist/js/owl.animate.js"></script>
+	<script src="dist/js/superfish.js"></script>
+	<script src="dist/js/waypoints.min.js"></script>
+	<script src="dist/js/jquery.counterup.min.js"></script>
+	<script src="dist/js/custom.js"></script>
+</body>
+
+</html>
